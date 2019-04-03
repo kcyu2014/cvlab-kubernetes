e925fea14e96f09450d05e43212816272f646ab5
kcyu2014 <yukaicheng@hotmail.com>
2019-04-03 11:42:28 +0200

    update the testing script

--- examples/sequential-jobs/test-cases/binbash-multiple/test.sh
+++ examples/sequential-jobs/test-cases/binbash-multiple/test.sh
@@ -6,12 +6,16 @@ PATH=$HOME/Dropbox/git/cvlab-kubernetes/examples/sequential-jobs:$PATH
 #. submit_jobs.sh old/begin.yaml 0.005 3
 if [ -z "$1" ]
 then
-. submit_jobs.sh test-binbash.yaml 0.1 10
-. submit_jobs.sh test-binbash-2.yaml 0.1 10
-. submit_jobs.sh test-binbash-3.yaml 0.1 10
+#. submit_jobs.sh test-binbash.yaml 0.1 10
+#. submit_jobs.sh test-binbash-2.yaml 0.1 10
+#. submit_jobs.sh test-binbash-3.yaml 0.1 10
+. submit_jobs.sh test-binbash-4.yaml 0.1 10
+. submit_jobs.sh test-binbash-5.yaml 0.1 10
 else
 kubectl delete -f test-binbash.yaml.job
 kubectl delete -f test-binbash-2.yaml.job
 kubectl delete -f test-binbash-3.yaml.job
+kubectl delete -f test-binbash-4.yaml.job
+kubectl delete -f test-binbash-5.yaml.job
 
 fi
